150ef44ba5991b294e55ca1317703c5d61634d86
claudiusalp <alptekin.orbay@gmail.com>
2017-05-22 00:20:06 +0300

    Comments added.

--- graph.py
+++ graph.py
@@ -5,12 +5,12 @@ import numpy as np
 
 stop_words = get_stop_words('english')
 
-
+# lower and tokenize
 def tokenize(corpus):
     all_words = re.findall(r'\w+', corpus.lower())
     return all_words
 
-
+# add dictionary if not exists.
 def add_dictionary(word, dictionary):
     if not(word is dictionary):
         dictionary[word] = 0
@@ -19,12 +19,14 @@ def add_dictionary(word, dictionary):
 def create_dictionary(path, dictionary):
     for file in listdir(path):
         if file.endswith(".txt"):
+            # only txt files
             corpus = open(path+'/'+file).read()
             word_list = tokenize(corpus)
             for word in word_list:
                     add_dictionary(word, dictionary)
 
 
+# hash all words for accesing easily.
 def index_dict(dictionary):
     counter = 0;
     inv_dict = []
@@ -37,9 +39,11 @@ def index_dict(dictionary):
 
 def create_graph(path, dictionary):
     create_dictionary(path,dictionary)
+    ## Initial parameters.
     window_size = 10;
     interval = int(window_size/2);
     size = len(dictionary)
+    ###
     address_map = index_dict(dictionary)
     t_matrix = np.zeros((size, size), dtype=np.float32)
     for file in listdir(path):
@@ -64,31 +68,40 @@ def create_graph(path, dictionary):
 
 
 def learn(path, dictionary):
+    # Initial parameters
     random_jump = 0.1
     top = 100
     eps = 0.0001
     [matrix, address_map] = create_graph(path, dictionary)
     size = np.shape(matrix)[0]
+    # normalize the matrix
     row_sums = matrix.sum(axis=1)
     matrix = matrix / row_sums[:, np.newaxis]
+    # add random walk probability.
     matrix *= (1-random_jump)
     matrix += random_jump / size
+    # initial vector is defined.
     initial_vector = np.random.randn(size)
     initial_vector = np.abs(initial_vector)
+    # normalized the vector.
     initial_vector = initial_vector / np.linalg.norm(initial_vector)
     print("Iteration Starts")
     for i in range(200):
+        # iteration
         initial_vector_x = initial_vector.dot(matrix)
         print("Iteration :", i)
+        # if norm-2 difference is below eps halt.
         if np.linalg.norm(initial_vector_x-initial_vector) < eps:
             print("Converged.")
             break
         initial_vector = initial_vector_x
+    # sort top to bottom order.
     index = initial_vector.argsort()[::-1][:size]
     c = 0;
     print("---Words----")
     for i in range(size):
         word = address_map[index[i]];
+        ## elimitate stop words.
         if word in stop_words:
             continue
         print(word)
